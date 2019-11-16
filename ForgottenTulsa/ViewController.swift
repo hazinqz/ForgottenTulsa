a508e5953319209d5dd93a7040b7a95bb8a3aa0e
Jeffery Mason <jefferymason72@gmail.com>
2019-11-16 10:21:42 -0600

    Initial Commit

---
+++ ForgottenTulsa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForgottenTulsa
+//
+//  Created by Jeffery Mason on 11/16/19.
+//  Copyright © 2019 Jeffery Mason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
