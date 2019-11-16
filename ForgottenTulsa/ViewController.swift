f0677898e569b6eebbc824faccb27d4ee2f3673e
Jeffery Mason <jefferymason72@gmail.com>
2019-11-16 11:51:17 -0600

    Set initial location to DownTown Tulsa in ViewController.

--- ForgottenTulsa/ViewController.swift
+++ ForgottenTulsa/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      // Set initial location to Tulsa
+        let initialLocation = CLLocation(latitude: 36.153980, longitude: -95.992775)
+
+        let regionRadius: CLLocationDistance = 1000
+        func centerMapOnLocation(location: CLLocation) {
+            let coordinateRegoin = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+            mapView.setRegion(coordinateRegoin, animated: true)
+
+        }
+        
+        centerMapOnLocation(location: initialLocation)
     }
 
 
